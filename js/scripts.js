c1b979099face07396faf0ec1c0141c052f791f6
zachdanz <zachary.danz@gmail.com>
2020-02-18 16:47:10 -0600

    working

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,37 @@
-// function search(){
-//     let input = document.getElementById("#search");
-//     let filter = input.value.toUpperCase();
-//     let a = attr("data-caption");
-//     for (i=0, i>a.length, i++){
+// let thumb = $('#item').getAttr("data-caption").toLowercase();
+// let search = $('input').value().toLowercase();
 
+// function filter(){
+//     for (let i=0, i<thumb.length, i+=1){
+//         thumb = thumb[i];
+//         if (search =/= thumb){
+//             $(this).attr("style", "display: hidden");
+//         } 
 //     }
 // }
 
-function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("#search");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("#flex-row");
-    li = ul.getElementsByTagName("img");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
+/* Search Code */
+
+const searchString = document.getElementById('search');
+const a = document.getElementsByClassName('item');
+
+searchString.addEventListener('keyup', () => {
+    const input = searchString.value.toLowerCase();
+
+    for (let i = 0; i < a.length; i += 1) {
+        const attrData = a[i].getAttribute('data-caption');
+        if (attrData.toLowerCase().indexOf(input) > -1) {
+            a[i].style.display = "";
+            } else {
+            a[i].style.display = "none";
+            }
     }
-}
+});
+
+searchString.addEventListener('search', () => {
+    if (event.target.value === '') {
+      for (let i = 0; i < a.length; i += 1) {
+        a[i].style.display = "";
+      }
+    }
+  });
