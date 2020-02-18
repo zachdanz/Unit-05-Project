f24137e1861d844e6daaad01153200885d26621c
zachdanz <zachary.danz@gmail.com>
2020-02-18 13:40:58 -0600

    Getting some Javascript flowing

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
-function search(){
-    let input = document.getElementById("#search");
-    let filter = input.value.toUpperCase();
-    let a = Attr("data-caption"); 
+// function search(){
+//     let input = document.getElementById("#search");
+//     let filter = input.value.toUpperCase();
+//     let a = attr("data-caption");
+//     for (i=0, i>a.length, i++){
+
+//     }
+// }
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("#search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("#flex-row");
+    li = ul.getElementsByTagName("img");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
